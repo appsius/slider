f35f6646effee9c1aa57afda5bf883500bb87a5e
apsius <38158216+appsius@users.noreply.github.com>
2021-03-01 08:33:57 +0300

    set html

---
+++ app.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('.container');
+const slider = document.querySelector('.slider');
+const before = document.querySelector('.img__container--before');
+const after = document.querySelector('.img__container--after');
+
+const glide = e => {
+	const posX = e.layerX;
+	const containerWidth = container.offsetWidth;
+
+	console.log(posX, containerWidth);
+
+	before.style.width = `${posX}px`;
+	slider.style.left = `${posX}px`;
+
+	if (posX < 50) {
+		before.style.width = 0;
+		slider.style.left = 0;
+	}
+
+	if (posX + 50 > containerWidth) {
+		before.style.width = `${containerWidth}px`;
+		slider.style.left = `${containerWidth}px`;
+	}
+};
+
+container.addEventListener('mousemove', glide);
